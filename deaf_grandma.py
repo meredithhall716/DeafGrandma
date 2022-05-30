e185b488c49061e0038d61659ab648c3652c943d
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-30 19:00:37 -0400

    first

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,4 +1,26 @@
+from operator import truediv
+
+
 def deaf_grandma():
+    words = True
+    goodbyes = 0
+
+    while(words):
+        user_input = input("Hey Kid!")
+
+        if user_input == "":
+            input("WHAT!?")
+        elif user_input.upper() != user_input:
+            input("SPEAK UP, KID!")
+        elif user_input == "GOODBYE!":
+            goodbyes += 1
+            if(goodbyes == 1):
+                input("LEAVING SO SOON?")
+            elif(goodbyes == 2):
+                input("LATER SKATER!")
+                words == False
+        elif user_input.upper() == user_input:
+            input("NO, NOT SINCE 1946!")
 
 
 
